4b48c10bd3ffaf3b93422e6f0e8d3fa062e09fb4
Yuliya1913 <115290066+Yuliya1913@users.noreply.github.com>
2023-03-21 13:04:29 +0000

    Deploying to gh-pages from @ Yuliya1913/goit-js-hw-09@54b3a8917d0155b205dcb843a5d1f06181df82c4 🚀

---
+++ 01-color-switcher.97e23adc.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body");console.log(e);const t=document.querySelector("[data-start]");console.log(t);const o=document.querySelector("[data-stop]");let l;console.log(o);t.addEventListener("click",(()=>{l=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled",!0)}),1e3)})),o.addEventListener("click",(()=>{clearInterval(l),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.97e23adc.js.map
